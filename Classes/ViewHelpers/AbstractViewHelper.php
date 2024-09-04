eb5a6916fbed3d3772a83000c78c84d60c08ab01
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 13:56:11 +0200

    Create AbstractViewHelper.php

---
+++ Classes/ViewHelpers/AbstractViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * AbstractViewHelper.
+ */
+declare(strict_types=1);
+
+namespace Checkit\VerowaApiconnector\ViewHelpers;
+
+/**
+ * AbstractViewHelper.
+ */
+abstract class AbstractViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+}
