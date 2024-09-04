ce0212945bf24662921d835f32524a0ade7e6567
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 22:25:40 +0200

    Tca.php aktualisieren

--- Classes/Userfuncs/Tca.php
+++ Classes/Userfuncs/Tca.php
@@ -1,5 +1,5 @@
 <?php
-namespace Checkit\VerowaApiconnector\Userfuncs;
+namespace Checkit\VerowaImportapi\Userfuncs;
 
 /**
  * This file is part of the TYPO3 CMS project.
@@ -21,30 +21,33 @@ namespace Checkit\VerowaApiconnector\Userfuncs;
  * @package TYPO3
  * @subpackage tx_examples
  */
-class Tca {
+class Tca
+{
 
 	/**
-	 * This method creates custom titles for the records of the tx_verowaapiconnector_domain_model_event table
+	 * This method creates custom titles for the records of the tx_verowaimportapi_domain_model_event table
 	 *
 	 * @param array $parameters Parameters used to identify the current record
 	 * @param object $parentObject Calling object
 	 * @return void
 	 */
-	public function eventBeTitle(&$parameters, $parentObject) {
+	public function eventBeTitle(&$parameters, $parentObject)
+	{
 		$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
 		$newTitle = $record['event_id'] . ' - ' . $record['title']; // Kombiniere event_id und title mit einem Trennzeichen
 		// $newTitle .= ' (' . substr(strip_tags($record['poem']), 0, 10) . '...)';
 		$parameters['title'] = $newTitle;
 	}
-
-    /**
-	 * This method creates custom titles for the records of the tx_verowaapiconnector_domain_model_layer table
+	
+	/**
+	 * This method creates custom titles for the records of the tx_verowaimportapi_domain_model_layer table
 	 *
 	 * @param array $parameters Parameters used to identify the current record
 	 * @param object $parentObject Calling object
 	 * @return void
 	 */
-	public function layerBeTitle(&$parameters, $parentObject) {
+	public function layerBeTitle(&$parameters, $parentObject)
+	{
 		$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
 		$newTitle = $record['layer_id'] . ' - ' . $record['layer_name']; // Kombiniere event_id und title mit einem Trennzeichen
 		// $newTitle .= ' (' . substr(strip_tags($record['poem']), 0, 10) . '...)';
@@ -52,13 +55,14 @@ class Tca {
 	}
 	
 	/**
-	 * This method creates custom titles for the records of the tx_verowaapiconnector_domain_model_room table
+	 * This method creates custom titles for the records of the tx_verowaimportapi_domain_model_room table
 	 *
 	 * @param array $parameters Parameters used to identify the current record
 	 * @param object $parentObject Calling object
 	 * @return void
 	 */
-	public function roomBeTitle(&$parameters, $parentObject) {
+	public function roomBeTitle(&$parameters, $parentObject)
+	{
 		$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
 		$newTitle = $record['room_id'] . ' - ' . $record['location_name'] . ' - ' . $record['room_name']; // Kombiniere event_id und title mit einem Trennzeichen
 		// $newTitle .= ' (' . substr(strip_tags($record['poem']), 0, 10) . '...)';
@@ -66,13 +70,14 @@ class Tca {
 	}
 	
 	/**
-	 * This method creates custom titles for the records of the tx_verowaapiconnector_domain_model_person table
+	 * This method creates custom titles for the records of the tx_verowaimportapi_domain_model_person table
 	 *
 	 * @param array $parameters Parameters used to identify the current record
 	 * @param object $parentObject Calling object
 	 * @return void
 	 */
-	public function personBeTitle(&$parameters, $parentObject) {
+	public function personBeTitle(&$parameters, $parentObject)
+	{
 		$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
 		$newTitle = $record['person_id'] . ' - ' . $record['name']; // Kombiniere event_id und title mit einem Trennzeichen
 		// $newTitle .= ' (' . substr(strip_tags($record['poem']), 0, 10) . '...)';
