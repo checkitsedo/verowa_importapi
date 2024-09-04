ebd3f94d8b7d582d3a1f756cbee70ee2e87c44da
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 23:47:29 +0200

    tt_content.php aktualisieren

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,27 +2,27 @@
 defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'VerowaApiconnector',
+    'VerowaImportapi',
     'Eventlist',
-    'LLL:EXT:verowa_apiconnector/Resources/Private/Language/locallang.xlf:plugin.eventlist.title',
-	'EXT:verowa_apiconnector/Resources/Public/Icons/user_plugin_eventlist.svg'
+    'LLL:EXT:verowa_importapi/Resources/Private/Language/locallang.xlf:plugin.eventlist.title',
+	'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventlist.svg'
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'VerowaApiconnector',
+    'VerowaImportapi',
     'Eventdetails',
-    'LLL:EXT:verowa_apiconnector/Resources/Private/Language/locallang.xlf:plugin.eventdetails.title',
-	'EXT:verowa_apiconnector/Resources/Public/Icons/user_plugin_eventdetails.svg'
+    'LLL:EXT:verowa_importapi/Resources/Private/Language/locallang.xlf:plugin.eventdetails.title',
+	'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventdetails.svg'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaapiconnector_eventlist'] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaimportapi_eventlist'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'verowaapiconnector_eventlist',
-    'FILE:EXT:verowa_apiconnector/Configuration/FlexForms/PluginEventlist.xml'
+    'verowaimportapi_eventlist',
+    'FILE:EXT:verowa_importapi/Configuration/FlexForms/PluginEventlist.xml'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaapiconnector_eventdetails'] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaimportapi_eventdetails'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'verowaapiconnector_eventdetails',
-    'FILE:EXT:verowa_apiconnector/Configuration/FlexForms/PluginEventdetails.xml'
+    'verowaimportapi_eventdetails',
+    'FILE:EXT:verowa_importapi/Configuration/FlexForms/PluginEventdetails.xml'
 );
