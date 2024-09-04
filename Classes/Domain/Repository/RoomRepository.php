9e8588d68a283c9132a5b1652d36bd541c41f2b7
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 13:48:55 +0200

    Create RoomRepository.php

---
+++ Classes/Domain/Repository/RoomRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Checkit\VerowaApiconnector\Domain\Repository;
+
+
+/**
+ * This file is part of the "Verowa API Connector" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2021 Dominik Senti <info@senti.lu>, checkit senti.lu
+ */
+
+/**
+ * The repository for Rooms
+ */
+class RoomRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+}
