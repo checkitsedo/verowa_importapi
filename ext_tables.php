941676e10416b67af658eab22ffef124b37e5ed6
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 12:47:59 +0200

    Create ext_tables.php

---
+++ ext_tables.php
@@ -0,0 +1,49 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+	static function()
+	{
+		
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+			'tx_verowaapiconnector_domain_model_event',
+			'EXT:verowa_apiconnector/Resources/Private/Language/locallang_csh_tx_verowaapiconnector_domain_model_event.xlf'
+		);
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+			'tx_verowaapiconnector_domain_model_event'
+		);
+		
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+			'tx_verowaapiconnector_domain_model_person',
+			'EXT:verowa_apiconnector/Resources/Private/Language/locallang_csh_tx_verowaapiconnector_domain_model_person.xlf'
+		);
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+			'tx_verowaapiconnector_domain_model_person'
+		);
+
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+			'tx_verowaapiconnector_domain_model_targetgroup',
+			'EXT:verowa_apiconnector/Resources/Private/Language/locallang_csh_tx_verowaapiconnector_domain_model_targetgroup.xlf'
+		);
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+			'tx_verowaapiconnector_domain_model_targetgroup'
+		);
+
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+			'tx_verowaapiconnector_domain_model_layer',
+			'EXT:verowa_apiconnector/Resources/Private/Language/locallang_csh_tx_verowaapiconnector_domain_model_layer.xlf'
+		);
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+			'tx_verowaapiconnector_domain_model_layer'
+		);
+		
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+			'tx_verowaapiconnector_domain_model_room',
+			'EXT:verowa_apiconnector/Resources/Private/Language/locallang_csh_tx_verowaapiconnector_domain_model_room.xlf'
+		);
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+			'tx_verowaapiconnector_domain_model_room'
+		);
+		
+	}
+);
