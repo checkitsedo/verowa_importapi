202c3aff17646c9d7415257fc85e87fe194d6fe0
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 16:05:47 +0200

    Create sys_template.php

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('verowa_apiconnector', 'Configuration/TypoScript', 'Verowa API Connector');
