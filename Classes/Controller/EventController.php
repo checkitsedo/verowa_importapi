f8d6a0604eb703ad59022c0c1001982e9410268e
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 21:44:15 +0200

    EventController.php aktualisieren

--- Classes/Controller/EventController.php
+++ Classes/Controller/EventController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Checkit\VerowaApiconnector\Controller;
+namespace Checkit\VerowaImportapi\Controller;
 
 /*
  * This file is part of the "Verowa API Connector" Extension for TYPO3 CMS.
@@ -13,8 +13,8 @@ namespace Checkit\VerowaApiconnector\Controller;
  * (c) 2021 Dominik Senti <info@senti.lu>, checkit senti.lu
  */
 
-use Checkit\VerowaApiconnector\Domain\Model\Event;
-use Checkit\VerowaApiconnector\Domain\Repository\EventRepository;
+use Checkit\VerowaImportapi\Domain\Model\Event;
+use Checkit\VerowaImportapi\Domain\Repository\EventRepository;
 use GeorgRinger\NumberedPagination\NumberedPagination;
 use TYPO3\CMS\Core\Pagination\SimplePagination;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
@@ -28,54 +28,55 @@ class EventController extends ActionController
 {
 
 	/**
- 	 * eventRepository
-   	 *
-     * @var \Checkit\VerowaApiconnector\Domain\Repository\EventRepository
-     */
+	 * eventRepository
+	 *
+		* @var \Checkit\VerowaImportapi\Domain\Repository\EventRepository
+		*/
 	protected $eventRepository = null;
 
 	/**
- 	 * @param \Checkit\VerowaApiconnector\Domain\Repository\EventRepository $eventRepository
-   	 */
-	public function injectEventRepository(\Checkit\VerowaApiconnector\Domain\Repository\EventRepository $eventRepository)
+	 * @param \Checkit\VerowaImportapi\Domain\Repository\EventRepository $eventRepository
+		*/
+	public function injectEventRepository(\Checkit\VerowaImportapi\Domain\Repository\EventRepository $eventRepository)
 	{
 		$this->eventRepository = $eventRepository;
 	}
 
 	/**
- 	 * action list
-   	 *
-     	 * @return string|object|null|void
-       	 */
+	 * action list
+		*
+		* @return string|object|null|void
+		*/
 	public function listAction()
 	{
 		$this->cacheService->clearPageCache($pid);
-
+		
 		$events = $this->eventRepository->findAll();
-
+		
 		// pagination
 		$itemsPerPage = 10;
 		$maximumLinks = 5;
 		$currentPage = $this->request->hasArgument('currentPage') ? (int)$this->request->getArgument('currentPage') : 1;
 		$paginator = new \TYPO3\CMS\Extbase\Pagination\QueryResultPaginator($events, $currentPage, $itemsPerPage);
 		$pagination = new \GeorgRinger\NumberedPagination\NumberedPagination($paginator, $maximumLinks);
-
+		
 		$this->view->assign('pagination', [
 			'paginator' => $paginator,
 			'pagination' => $pagination,
 		]);
 	}
-
+	
 	/**
- 	 * action show
-   	 *
-     	 * @param \Checkit\VerowaApiconnector\Domain\Model\Event $event
-       	 * @return string|object|null|void
-	 */
-	public function showAction(\Checkit\VerowaApiconnector\Domain\Model\Event $event)
+	 * action show
+		*
+		* @param \Checkit\VerowaImportapi\Domain\Model\Event $event
+		* @return string|object|null|void
+		*/
+	public function showAction(\Checkit\VerowaImportapi\Domain\Model\Event $event)
 	{
 		$this->view->assign('event', $event);
 	}
+	
 	protected function getErrorFlashMessage()
 	{
 		return false;
