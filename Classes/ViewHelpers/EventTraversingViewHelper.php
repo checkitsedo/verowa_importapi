db8702077e49b596b27a35df39ef5adc74b521c0
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-04 13:58:42 +0200

    Create EventTraversingViewHelper.php

---
+++ Classes/ViewHelpers/EventTraversingViewHelper.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Index traversing.
+ */
+declare(strict_types=1);
+
+namespace Checkit\VerowaApiconnector\ViewHelpers;
+
+use Checkit\VerowaApiconnector\Domain\Model\Event;
+use Checkit\VerowaApiconnector\Domain\Repository\EventRepository;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+
+/**
+ * Index traversing.
+ *
+ * == Examples ==
+ *
+ * <code title="Traversing thru future and past occurings of the event">
+ * {namespace checkit=Checkit\VerowaApiconnector\ViewHelpers}
+ * <f:for each="{checkit:eventTraversing(event:'{event}', limit:'{settings.limitrecord}', sort: 'ASC')}" as="futureEvent">
+ *  <f:debug>{futureEvent}</f:debug>
+ * </f:for>
+ * </code>
+ */
+class EventTraversingViewHelper extends AbstractViewHelper
+{
+    /**
+     * Init arguments.
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('event', Event::class, '', true);
+        $this->registerArgument('limit', 'int', '', false, 100);
+        $this->registerArgument('sort', 'string', '', false, QueryInterface::ORDER_ASCENDING);
+    }
+
+    /**
+     * Render method.
+     *
+     * @return array
+     */
+    public function render()
+    {
+        $eventRepository = GeneralUtility::makeInstance(ObjectManager::class)->get(EventRepository::class);
+
+        return $eventRepository->findByTraversing(
+            $this->arguments['event'],
+            (int)$this->arguments['limit'],
+            $this->arguments['sort'],
+        );
+    }
+}
