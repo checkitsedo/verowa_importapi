f7a22e228bbdbba2517efbc6e5abd519322ca5ae
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 19:15:11 +0200

    ext_tables.php aktualisieren

--- ext_tables.php
+++ ext_tables.php
@@ -1,49 +1,67 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-	static function()
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+defined('TYPO3') or die();
+
+(static function()
 	{
 		
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+		$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+		
+		ExtensionManagementUtility::addLLrefForTCAdescr(
 			'tx_verowaimportapi_domain_model_event',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_event.xlf'
 		);
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaimportapi_domain_model_event'
-		);
+		if ($versionInformation->getMajorVersion() < 12) {
+			ExtensionManagementUtility::allowTableOnStandardPages(
+				'tx_verowaimportapi_domain_model_event',
+			);
+		}
 		
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+		ExtensionManagementUtility::addLLrefForTCAdescr(
 			'tx_verowaimportapi_domain_model_person',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_person.xlf'
 		);
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaimportapi_domain_model_person'
-		);
+		if ($versionInformation->getMajorVersion() < 12) {
+			ExtensionManagementUtility::allowTableOnStandardPages(
+				'tx_verowaimportapi_domain_model_person',
+			);
+		}
 
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+		ExtensionManagementUtility::addLLrefForTCAdescr(
 			'tx_verowaimportapi_domain_model_targetgroup',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_targetgroup.xlf'
 		);
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaimportapi_domain_model_targetgroup'
-		);
+		if ($versionInformation->getMajorVersion() < 12) {
+			ExtensionManagementUtility::allowTableOnStandardPages(
+				'tx_verowaimportapi_domain_model_targetgroup',
+			);
+		}
 
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+		ExtensionManagementUtility::addLLrefForTCAdescr(
 			'tx_verowaimportapi_domain_model_layer',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_layer.xlf'
 		);
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaimportapi_domain_model_layer'
-		);
+		if ($versionInformation->getMajorVersion() < 12) {
+			ExtensionManagementUtility::allowTableOnStandardPages(
+				'tx_verowaimportapi_domain_model_layer',
+			);
+		}
 		
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+		ExtensionManagementUtility::addLLrefForTCAdescr(
 			'tx_verowaimportapi_domain_model_room',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_room.xlf'
 		);
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaimportapi_domain_model_room'
-		);
+		if ($versionInformation->getMajorVersion() < 12) {
+			ExtensionManagementUtility::allowTableOnStandardPages(
+				'tx_verowaimportapi_domain_model_room',
+			);
+		}
 		
 	}
-);
+)();
