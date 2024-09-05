f9ef224a3aa1bd31962b4b97736948633cc54d0f
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 21:17:14 +0200

    Icons.php aktualisieren

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -22,3 +22,27 @@ foreach ($icons as $identifier => $path) {
 };
 
 return[];
+
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    // Icon identifier
+    'verowa_importapi-plugin-eventlist' => [
+        // Icon provider class
+        'provider' => SvgIconProvider::class,
+        // The source SVG for the SvgIconProvider
+        'source' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventlist.svg',
+    ],
+    'verowa_importapi-plugin-eventdetails' => [
+        // Icon provider class
+        'provider' => SvgIconProvider::class,
+        // The source SVG for the SvgIconProvider
+        'source' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventdetails.svg',
+    ],
+];
+
