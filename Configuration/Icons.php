a40cc3874a24db4701c79e995b12af807da62fc8
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 20:59:28 +0200

    Icons.php aktualisieren

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -8,8 +8,8 @@ use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 return [
     $icons = [
         'verowa_importapi-plugin-eventlist' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventlist.svg',
-		'verowa_importapi-plugin-eventdetails' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventdetails.svg',
-	];
+        'verowa_importapi-plugin-eventdetails' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventdetails.svg',
+    ]
     
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
     foreach ($icons as $identifier => $path) {
