6bc02b82746eeda628ae204133e81ed7e3cf8f8d
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 22:26:36 +0200

    ext_tables.php aktualisieren

--- ext_tables.php
+++ ext_tables.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Localization\LanguageService;
 
 defined('TYPO3') or die();
 
@@ -13,55 +14,56 @@ defined('TYPO3') or die();
 		
 		$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 		
-		ExtensionManagementUtility::addLLrefForTCAdescr(
+		// Helper function to add descriptions based on TYPO3 version
+		function addTableDescriptions(string $table, string $filePath): void
+		{
+			if (class_exists(LanguageService::class)) {
+				$GLOBALS['TCA_DESCR'][$table]['refs'] = $filePath;
+			} else {
+				ExtensionManagementUtility::addLLrefForTCAdescr(
+					$table,
+					$filePath
+				);
+			}
+		}
+		
+		addTableDescriptions(
 			'tx_verowaimportapi_domain_model_event',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_event.xlf'
 		);
-		if ($versionInformation->getMajorVersion() < 12) {
-			ExtensionManagementUtility::allowTableOnStandardPages(
-				'tx_verowaimportapi_domain_model_event',
-			);
-		}
 		
-		ExtensionManagementUtility::addLLrefForTCAdescr(
+		addTableDescriptions(
 			'tx_verowaimportapi_domain_model_person',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_person.xlf'
 		);
-		if ($versionInformation->getMajorVersion() < 12) {
-			ExtensionManagementUtility::allowTableOnStandardPages(
-				'tx_verowaimportapi_domain_model_person',
-			);
-		}
-
-		ExtensionManagementUtility::addLLrefForTCAdescr(
+		
+		addTableDescriptions(
 			'tx_verowaimportapi_domain_model_targetgroup',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_targetgroup.xlf'
 		);
-		if ($versionInformation->getMajorVersion() < 12) {
-			ExtensionManagementUtility::allowTableOnStandardPages(
-				'tx_verowaimportapi_domain_model_targetgroup',
-			);
-		}
-
-		ExtensionManagementUtility::addLLrefForTCAdescr(
+		
+		addTableDescriptions(
 			'tx_verowaimportapi_domain_model_layer',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_layer.xlf'
 		);
-		if ($versionInformation->getMajorVersion() < 12) {
-			ExtensionManagementUtility::allowTableOnStandardPages(
-				'tx_verowaimportapi_domain_model_layer',
-			);
-		}
 		
-		ExtensionManagementUtility::addLLrefForTCAdescr(
+		addTableDescriptions(
 			'tx_verowaimportapi_domain_model_room',
 			'EXT:verowa_importapi/Resources/Private/Language/locallang_csh_tx_verowaimportapi_domain_model_room.xlf'
 		);
-		if ($versionInformation->getMajorVersion() < 12) {
-			ExtensionManagementUtility::allowTableOnStandardPages(
-				'tx_verowaimportapi_domain_model_room',
-			);
-		}
 		
+		// Allow tables on standard pages for TYPO3 versions < 12
+		if ($versionInformation->getMajorVersion() < 12) {
+			$tables = [
+				'tx_verowaimportapi_domain_model_event',
+				'tx_verowaimportapi_domain_model_person',
+				'tx_verowaimportapi_domain_model_targetgroup',
+				'tx_verowaimportapi_domain_model_layer',
+				'tx_verowaimportapi_domain_model_room'
+			];
+			foreach ($tables as $table) {
+				ExtensionManagementUtility::allowTableOnStandardPages($table);
+			}
+		}
 	}
 )();
