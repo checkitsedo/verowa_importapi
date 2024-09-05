ed5ff1fd831f1be77f237fc04f64cdc7bbbba243
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 20:11:23 +0200

    sys_template.php aktualisieren

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,9 @@
 <?php
-defined('TYPO3_MODE') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('verowa_importapi', 'Configuration/TypoScript', 'Verowa API Connector');
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'verowa_importapi',
+    'Configuration/TypoScript/',
+    'Verowa API Connector'
+);
