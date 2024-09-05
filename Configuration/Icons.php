b13f2b189b9b10e71944a8ad2518579de4708517
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 21:10:16 +0200

    Icons.php aktualisieren

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -4,19 +4,21 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
 
-return [
-    $icons = [
-        'verowa_importapi-plugin-eventlist' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventlist.svg',
-        'verowa_importapi-plugin-eventdetails' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventdetails.svg'
-    ];
-    
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    foreach ($icons as $identifier => $path) {
-        $iconRegistry->registerIcon(
-            $identifier,
-            SvgIconProvider::class,
-            ['source' => $path]
-        );
-    };
+$icons = [
+    'verowa_importapi-plugin-eventlist' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventlist.svg',
+    'verowa_importapi-plugin-eventdetails' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventdetails.svg'
 ];
+    
+$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+foreach ($icons as $identifier => $path) {
+    $iconRegistry->registerIcon(
+        $identifier,
+        SvgIconProvider::class,
+        ['source' => $path]
+    );
+};
+
+return[];
