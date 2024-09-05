541f03ffd5023d11da5a972b3f8d17a512996db5
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 19:37:47 +0200

    ext_localconf.php aktualisieren

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,15 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-	static function()
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+defined('TYPO3') or die();
+
+	(static function()
 	{
-		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		ExtensionUtility::configurePlugin(
 			'VerowaImportapi',
 			'Eventlist',
 			[
@@ -16,7 +21,7 @@ call_user_func(
 			]
 		);
 		
-		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		ExtensionUtility::configurePlugin(
 			'VerowaImportapi',
 			'Eventdetails',
 			[
@@ -26,10 +31,6 @@ call_user_func(
 			[
 				\Checkit\VerowaImportapi\Controller\EventController::class => ''
 			]
-		);
-
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-			'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:verowa_importapi/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig">'
 		);
 
 		$icons = [
@@ -45,34 +46,5 @@ call_user_func(
 				['source' => $path]
 			);
 		};
-		
-		// wizards
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-			'mod {
-				wizards.newContentElement.wizardItems.plugins {
-					elements {
-						eventlist {
-							iconIdentifier = verowa_importapi-plugin-eventlist
-							title = LLL:EXT:verowa_importapi/Resources/Private/Language/locallang_db.xlf:tx_verowa_importapi_eventlist.name
-							description = LLL:EXT:verowa_importapi/Resources/Private/Language/locallang_db.xlf:tx_verowa_importapi_eventlist.description
-							tt_content_defValues {
-								CType = list
-								list_type = verowaimportapi_eventlist
-							}
-						}
-						eventdetails {
-							iconIdentifier = verowa_importapi-plugin-eventdetails
-							title = LLL:EXT:verowa_importapi/Resources/Private/Language/locallang_db.xlf:tx_verowa_importapi_eventdetails.name
-							description = LLL:EXT:verowa_importapi/Resources/Private/Language/locallang_db.xlf:tx_verowa_importapi_eventdetails.description
-							tt_content_defValues {
-								CType = list
-								list_type = verowaimportapi_eventdetails
-							}
-						}
-					}
-					show = *
-				}
-			}'
-		);
 	}
-);
+)();
