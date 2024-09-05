2e7c61e987cd2c473a5bfdfa4db29cc06c3175e5
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 20:56:55 +0200

    ext_localconf.php aktualisieren

--- ext_localconf.php
+++ ext_localconf.php
@@ -35,6 +35,8 @@ defined('TYPO3') or die();
 			]
 		);
 		
+		$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+		
 		// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
 		if ($versionInformation->getMajorVersion() < 12) {
 			ExtensionManagementUtility::addPageTSConfig(
