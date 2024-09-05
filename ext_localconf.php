cd93fbf3dce8c8515f0998e6f741032bee79d6f5
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-05 20:27:31 +0200

    ext_localconf.php aktualisieren

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -32,19 +33,12 @@ defined('TYPO3') or die();
 				\Checkit\VerowaImportapi\Controller\EventController::class => ''
 			]
 		);
-
-		$icons = [
-			'verowa_importapi-plugin-eventlist' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventlist.svg',
-			'verowa_importapi-plugin-eventdetails' => 'EXT:verowa_importapi/Resources/Public/Icons/user_plugin_eventdetails.svg',
-		];
 		
-		$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-		foreach ($icons as $identifier => $path) {
-			$iconRegistry->registerIcon(
-				$identifier,
-				\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-				['source' => $path]
+		// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+		if ($versionInformation->getMajorVersion() < 12) {
+			ExtensionManagementUtility::addPageTSConfig(
+				'@import "EXT:verowa_importapi/Configuration/page.tsconfig"'
 			);
-		};
+		}
 	}
 )();
